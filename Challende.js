f63949c2109d4c54b7776b76bbdb499fafdf9c56
Miss-Rey <audreychelangat2003@gmail.com>
2024-12-22 23:52:20 +0300

    JS Challenge

---
+++ Challende.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const { Departments } = require('../Models/Departments.model')
+const { Courses } = require('../Models/course.model')
+
+router.get('/', async (req, res) => {
+    try {
+        const departments = await Departments.find();
+        console.log(departments)
+        const result = await Promise.all(
+            departments.map(async (department) => {
+                const courses = await Courses.find({ departmentID: department._id, category: "short course" });
+                return {
+                    departmentName: department.departmentName,
+                    courses: courses,
+                };
+            })
+        );
+
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch department details' });
+    }
+});
+
+module.exports = router;
