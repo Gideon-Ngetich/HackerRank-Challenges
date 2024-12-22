aca82e52279c31ad14a9a3bfe5e7fafe6bc8059a
Gideon-Ngetich <sunbird08203@gmail.com>
2024-12-22 23:58:15 +0300

    JS Challenge

--- Challenge.js
+++ Challenge.js
@@ -43,4 +43,24 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/', async (req, res) => {
+    try {
+        const departments = await Departments.find();
+        console.log(departments)
+        const result = await Promise.all(
+            departments.map(async (department) => {
+                const courses = await Courses.find({ departmentID: department._id, category: "short course" });
+                return {
+                    departmentName: department.departmentName,
+                    courses: courses,
+                };
+            })
+        );
+
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch department details' });
+    }
+});
 module.exports = router;
